ea93b5dc3e729d3482a9059ce5800337492acc28
朴伟雄 <2355468258@qq.com>
2018-09-19 16:09:12 +0800

    LevelTreeOrder Do not valid

--- tree.go
+++ tree.go
@@ -1,7 +1,6 @@
 package leetcode
 
 import (
-	"golang.org/x/tools/container/intsets"
 	"math"
 )
 
@@ -45,4 +44,41 @@ func isValidBSTc(node *TreeNode, max int, min int) bool {
 		return false
 	}
 	return isValidBSTc(node.Left, node.Val, min) && isValidBSTc(node.Right, max, node.Val)
+}
+
+// leetcode: /explore/interview/card/top-interview-questions-easy/7/trees/50/
+
+func LevelTreeOrder(root *TreeNode) [][]int {
+	var result [][]int
+	last := root
+	nlast := root
+	queue := make([]*TreeNode, 0)
+	if root == nil {
+		return result
+	}
+	queue = append(queue, root)
+	level := make([]int, 0)
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+			nlast = node.Left
+		}
+
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+			nlast = node.Right
+		}
+
+		level = append(level, node.Val)
+
+		if node == last {
+			result = append(result, level)
+			level = make([]int, 0)
+			last = nlast
+		}
+	}
+	return result
 }
