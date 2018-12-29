06ae32a9c75c53ca7389297751e2e2938e782212
朴伟雄 <2355468258@qq.com>
2018-12-29 10:44:20 +0800

    链表两数相加

---
+++ medium/list.go
@@ -0,0 +1,119 @@
+// Copyright (c) 2018. weixiong piao, All Right Reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package medium
+
+import (
+	"fmt"
+	. "leetcode/structs"
+)
+
+func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	carry := 0
+	var result *ListNode
+	var head *ListNode
+
+	for l1 != nil || l2 != nil {
+		switch  {
+		case l1 != nil && l2 != nil:
+			sum := (l1.Val + l2.Val + carry) % 10
+			carry = (l1.Val + l2.Val + carry) / 10
+			tmpNode := ListNode{
+				Val:sum,
+				Next:nil,
+			}
+			if result == nil {
+				result = &tmpNode
+				head = result
+			} else {
+				result.Next = &tmpNode
+				result = result.Next
+			}
+			l1 = l1.Next
+			l2 = l2.Next
+		case carry != 0 && l1 != nil:
+			sum := (l1.Val + carry) % 10
+			carry = (l1.Val + carry) / 10
+			tmpNode := ListNode{
+				Val:sum,
+				Next:nil,
+			}
+
+			result.Next = &tmpNode
+			result = result.Next
+			l1 = l1.Next
+		case carry != 0 && l2 != nil:
+			sum := (l2.Val + carry) % 10
+			carry = (l2.Val + carry) / 10
+			tmpNode := ListNode{
+				Val:sum,
+				Next:nil,
+			}
+			carry = (l2.Val + carry) / 10
+			result.Next = &tmpNode
+			result = result.Next
+			l2 = l2.Next
+		case l2 != nil:
+			result.Next = l2
+			l2 = nil
+		case l1 != nil:
+			result.Next = l1
+			l1 = nil
+		default:
+			fmt.Println("something wrong!")
+		}
+	}
+
+	if carry != 0 {
+		tmpNode := ListNode{
+			Val:carry,
+			Next:nil,
+		}
+		result.Next = &tmpNode
+	}
+	return head
+}
+
+// leetcode's God algorithm
+// smart code
+
+func AddTwoNumbers2(l1 *ListNode, l2 *ListNode) *ListNode {
+	var head *ListNode
+	var tail *ListNode
+	var carry int
+	for l1 != nil || l2 != nil {
+		sum := carry
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		carry = sum / 10
+		val := sum % 10
+
+		node := &ListNode{val, nil}
+		if head == nil {
+			head = node
+		}
+		if tail == nil {
+			tail = node
+		} else {
+			tail.Next = node
+			tail = tail.Next
+		}
+	}
+
+	if carry != 0 {
+		node := &ListNode{carry, nil}
+		if head == nil {
+			 head = node
+		} else {
+			tail.Next = node
+		}
+	}
+	return head
+}
---
+++ structs/list.go
@@ -0,0 +1,42 @@
+// Copyright (c) 2018. weixiong piao, All Right Reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package structs
+
+import "fmt"
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func PrintList(l *ListNode)  {
+	for l.Next != nil {
+		fmt.Print(l.Val, "-")
+		l = l.Next
+	}
+	fmt.Println(l.Val)
+}
+
+func GenerateList(vals[]int) *ListNode {
+	var result *ListNode
+	var headList *ListNode
+	for _,v := range vals {
+		tempNode := &ListNode{v,nil}
+		if result == nil {
+			result =tempNode
+			headList = result
+		} else {
+			result.Next = tempNode
+			result = result.Next
+		}
+	}
+	return headList
+}
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
