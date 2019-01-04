2ba09d6f6ce65394c4c245520b55609f4f32e4b5
朴伟雄 <2355468258@qq.com>
2019-01-04 11:50:08 +0800

    岛屿数量

---
+++ medium/numIslands.go
@@ -0,0 +1,49 @@
+package medium
+
+func NumIslands(grid [][]byte) int {
+	rows := len(grid)
+	if rows == 0 {
+		return 0
+	}
+	cells := len(grid[0])
+	nums := 0
+	visited := make(map[int]map[int]bool, 0)
+	for i := 0; i < rows; i++ {
+		if visited[i] == nil {
+			visited[i] = make(map[int]bool, cells)
+		}
+		for j := 0; j < cells; j++ {
+			if visited[i][j] {
+				continue
+			}
+			if grid[i][j] == '1' {
+				nums++
+				numIslandsHelper(grid, &visited, i, j)
+			}
+		}
+	}
+	return nums
+}
+
+func numIslandsHelper(grid [][]byte, ma *map[int]map[int]bool, i, j int) {
+	//fmt.Println(m)
+	m := *ma
+	if grid[i][j] == '1' && !m[i][j] {
+		if m[i] == nil {
+			m[i] = make(map[int]bool, len(grid[0]))
+		}
+		m[i][j] = true
+		if j+1 < len(grid[0]) {
+			numIslandsHelper(grid, &m, i, j+1)
+		}
+		if j-1 >= 0 && !m[i][j-1] {
+			numIslandsHelper(grid, &m, i, j-1)
+		}
+		if i+1 < len(grid) {
+			numIslandsHelper(grid, &m, i+1, j)
+		}
+		if i-1 >=0 && !m[i-1][j] {
+			numIslandsHelper(grid, &m, i-1, j)
+		}
+	}
+}
