933232025659e6f29e641897294f598add1599a9
朴伟雄 <2355468258@qq.com>
2019-01-22 09:41:22 +0800

    电话按键组合

---
+++ medium/back-traceing.go
@@ -0,0 +1,98 @@
+// Copyright (c) 2019. weixiong piao, All Right Reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package medium
+
+func LetterCombinations(digits string) []string {
+	var res []string
+	//res := make([]string, 0)
+
+	m := map[rune][]string{
+		'2':[]string{"a","b","c"},
+		'3':[]string{"d","e","f"},
+		'4':[]string{"g","h","i"},
+		'5':[]string{"j","k","l"},
+		'6':[]string{"m","n","o"},
+		'7':[]string{"p","q","r","s"},
+		'8':[]string{"t","u","v"},
+		'9':[]string{"w","x","y","z"},
+	}
+
+	for _,md := range digits{
+		tm := m[md]
+		if res == nil {
+			for _,v := range tm {
+				res = append(res, v)
+
+			}
+		} else {
+			//var tmp []string
+			//tmp := make([]string, len(res))
+			//copy(tmp, res)
+			//fmt.Println(tmp)
+			//res = make([]string,0)
+			l := len(res)
+			for _, r := range res {
+				for _,v := range tm {
+					res = append(res, r + v)
+				}
+			}
+			res = res[l:]
+		}
+	}
+
+	return res
+}
+
+// some leetcoder's algorithm
+// 使用递归
+func letterCombinations(digits string) []string {
+
+	if len(digits) < 1 {
+		return []string{}
+	}
+
+	maps := make(map[int]string)
+	maps[2] = "abc"
+	maps[3] = "def"
+	maps[4] = "ghi"
+	maps[5] = "jkl"
+	maps[6] = "mno"
+	maps[7] = "pqrs"
+	maps[8] = "tuv"
+	maps[9] = "wxyz"
+
+	res := fun(digits,maps)
+
+	return res
+}
+
+func fun(digits string , maps map[int]string) (result []string){
+	if len(digits) < 1 {
+		return result
+	}
+
+	if len(digits) == 1{
+		ch := digits[0]
+		n := int(ch - '0')
+
+		str := maps[n]
+		for i:=0 ; i< len(str) ; i++{
+			result = append(result,str[i:i+1])
+		}
+		return result
+	}
+
+	data := fun(digits[1:],maps)
+	ch := digits[0]
+	n := int(ch - '0')
+
+	str := maps[n]
+	for i:=0 ; i< len(str) ; i++{
+		for j:=0;j<len(data);j++{
+			result = append(result,str[i:i+1] + data[j])
+		}
+	}
+	return result
+}
