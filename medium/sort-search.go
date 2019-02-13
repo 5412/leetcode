6d9c805ca29c1b6ef20fb4b4e730a1ab1913ee0d
朴伟雄 <2355468258@qq.com>
2019-02-13 11:51:35 +0800

    颜色排序

--- medium/sort-search.go
+++ medium/sort-search.go
@@ -6,7 +6,7 @@ package medium
 
 import "fmt"
 
-func SortColor(nums []int) {
+func sortColor(nums []int) {
 	if len(nums) < 2 {
 		return
 	}
@@ -57,4 +57,52 @@ func SortColor(nums []int) {
 			}
 		}
 	}
+}
+
+func SortColor(nums []int) {
+	var (
+		i,j int
+	)
+
+	for f:=0; f < len(nums); f++ {
+		switch nums[f] {
+		case 0:
+			nums[f] =  2
+			nums[j] = 1
+			nums[i] = 0
+			i++
+			j++
+			//
+		case 1:
+			nums[f] =  2
+			nums[j] =  1
+			j++
+		case 2:
+			//nums[j]  = 2
+		}
+	}
+}
+
+// leetcode God's algorithm
+func sortColors(a []int) {
+	i, j, k := 0, 0, len(a)-1
+
+	// for 循环中， nums[i:j] 中始终全是 1
+	// 循环结束后，
+	// nums[:i] 中全是 0
+	// nums[j:] 中全是 2
+	for j <= k {
+		switch a[j] {
+		case 0:
+			a[i], a[j] = a[j], a[i]
+			i++
+			j++
+		case 1:
+			j++
+		case 2:
+			a[j], a[k] = a[k], a[j]
+			k--
+		}
+	}
+
 }
