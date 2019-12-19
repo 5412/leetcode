1ecae3a6483fe103eede241b3a407f1abb2c208c
朴伟雄 <piaoweixiong@baijiahulian.com>
2019-12-19 20:34:24 +0800

    乘阶0个个数

--- medium/math_test.go
+++ medium/math_test.go
@@ -8,3 +8,12 @@ import (
 func TestIsHappy(t *testing.T) {
 	fmt.Println(IsHappy(19))
 }
+
+func TestTrailingZeroes(t *testing.T) {
+	fmt.Println(TrailingZeroes(3))
+	fmt.Println(TrailingZeroes(5))
+	fmt.Println(TrailingZeroes(6))
+	fmt.Println(TrailingZeroes(8))
+	fmt.Println(TrailingZeroes(10))
+	fmt.Println(TrailingZeroes(30))
+}
