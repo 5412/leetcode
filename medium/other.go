1dd75a58bfbed9920d2b84ec300c854cf8030534
朴伟雄 <piaoweixiong@baijiahulian.com>
2019-12-31 14:58:15 +0800

    task 调度

--- medium/other.go
+++ medium/other.go
@@ -1,6 +1,9 @@
 package medium
 
-import "strconv"
+import (
+	"sort"
+	"strconv"
+)
 
 /**
 不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
@@ -104,4 +107,127 @@ func evalRPN(tokens []string) int {
 		stack = append(stack, num)
 	}
 	return stack[0]
+}
+
+/**
+给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+示例 1:
+
+输入: [3,2,3]
+输出: 3
+示例 2:
+
+输入: [2,2,1,1,1,2,2]
+输出: 2
+ */
+
+func MajorityElement(nums []int) int {
+	m := make(map[int]int)
+	l := len(nums) / 2
+	for _, v := range nums {
+		m[v]++
+		if m[v] > l {
+			return v
+		}
+	}
+	return 0
+}
+
+/**
+解法三 摩尔投票法
+1980 年由 Boyer 和 Moore 两个人提出来的算法，英文是 Boyer-Moore Majority Vote Algorithm。
+
+算法思想很简单，但第一个想出来的人是真的强。
+
+我们假设这样一个场景，在一个游戏中，分了若干个队伍，有一个队伍的人数超过了半数。所有人的战力都相同，不同队伍的两个人遇到就是同归于尽，同一个队伍的人遇到当然互不伤害。
+
+这样经过充分时间的游戏后，最后的结果是确定的，一定是超过半数的那个队伍留在了最后。
+
+而对于这道题，我们只需要利用上边的思想，把数组的每个数都看做队伍编号，然后模拟游戏过程即可。
+
+group 记录当前队伍的人数，count 记录当前队伍剩余的人数。如果当前队伍剩余人数为 0，记录下次遇到的人的所在队伍号。
+
+作者：windliang
+链接：https://leetcode-cn.com/problems/majority-element/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-4-2/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+func majorityElement(nums []int) int {
+	ret := nums[0]
+	count := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == ret {
+			count++
+		} else {
+			count--
+			if count == 0 {
+				ret = nums[i]
+				count = 1
+			}
+		}
+	}
+	return ret
+}
+
+/**
+给定一个用字符数组表示的 CPU 需要执行的任务列表。其中包含使用大写的 A - Z 字母表示的26 种不同种类的任务。任务可以以任意顺序执行，并且每个任务都可以在 1 个单位时间内执行完。CPU 在任何一个单位时间内都可以执行一个任务，或者在待命状态。
+然而，两个相同种类的任务之间必须有长度为 n 的冷却时间，因此至少有连续 n 个单位时间内 CPU 在执行不同的任务，或者在待命状态。
+你需要计算完成所有任务所需要的最短时间。
+示例 1：
+输入: tasks = ["A","A","A","B","B","B"], n = 2
+输出: 8
+执行顺序: A -> B -> (待命) -> A -> B -> (待命) -> A -> B.
+注：
+任务的总个数为 [1, 10000]。
+n 的取值范围为 [0, 100]。
+ */
+func LeastInterval(tasks []byte, n int) int {
+	taskMap := make(map[byte]int)
+	mx := 0
+	for _, t := range tasks {
+		taskMap[t]++
+		mx = max(mx, taskMap[t])
+	}
+	cn := 0
+	for _, m := range taskMap {
+		if m == mx {
+			cn++
+		}
+	}
+
+	return max(len(tasks), (mx-1) * (n + 1) + cn)
+}
+
+func leastInterval(tasks []byte, n int) int {
+	taskCnt := len(tasks)
+	if taskCnt < 1 {
+		return 0
+	}
+	slots := make([]int, 26)
+
+	for i := range tasks {
+		slots[tasks[i] - 'A']++
+	}
+
+	sort.Ints(slots)
+	maxCnt := slots[25] -1
+	idles := maxCnt * n
+
+	for i := 24; i >= 0 && slots[i] > 0; i-- {
+		idles -= min2(slots[i], maxCnt)
+	}
+
+	if idles <= 0 {
+		return taskCnt
+	}
+	return taskCnt + idles
+}
+
+
+func min2(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
---
+++ medium/other_test.go
@@ -0,0 +1,10 @@
+package medium
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMajorityElement(t *testing.T) {
+	fmt.Println(MajorityElement([]int{1,1,2,3,4,5,6,6,6,6,6,6,6,6}))
+}
